5a147dddc8eba93f7ec49df8b24ca0acb812b6cc
Erfan9nine <erfan.agheli7979@gmail.com>
2023-11-08 00:43:21 -0800

    slider-img

--- src/js/img_slider.js
+++ src/js/img_slider.js
@@ -1,51 +1,45 @@
-let img = document.getElementsByTagName('img');
+let img = document.getElementsByTagName("img");
 
 const images = [
-    './src/img/pexels-dominika-gregušová-672532.jpg',
-    './src/img/pexels-kammeran-gonzalezkeola-9638689.jpg',
-    './src/img/pexels-pixabay-372470.jpg',
-    './src/img/pexels-trinity-kubassek-445109.jpg'
-]
-let mover ;
+  "./src/img/pexels-dominika-gregušová-672532.jpg",
+  "./src/img/pexels-kammeran-gonzalezkeola-9638689.jpg",
+  "./src/img/pexels-pixabay-372470.jpg",
+  "./src/img/pexels-trinity-kubassek-445109.jpg",
+];
+let mover;
 let counter = -1;
-function changer(){
-    mover = setInterval(()=>{
-        counter++;
-        img[0].src=images[counter]
-        if(counter<=-1 || counter >=3){
-            counter=0;
-
-        }
-        console.log(counter);
-},3000)
-    
-}
-function forward(){
-    clearInterval(mover)
+function changer() {
+  mover = setInterval(() => {
     counter++;
-    img[0].src=images[counter]
-    if(counter<=-1){
-        counter=2;
-
+    img[0].src = images[counter];
+    if (counter <= -1 || counter >= 3) {
+      counter = 0;
     }
-    if(counter >=3){
-        counter=-1
-    }
-    changer()
-    console.log(counter);
+  }, 3000);
 }
-function previous (){
-    clearInterval(mover)
- 
-    counter--;
-    img[0].src=images[counter]
-    if(counter<=-1){
-        counter=2;
+function forward() {
+  clearInterval(mover);
+  counter++;
+  img[0].src = images[counter];
+  if (counter <= -1) {
+    counter = 2;
+  }
+  if (counter >= 3) {
+    counter = -1;
+  }
+  changer();
+}
+function previous() {
+  clearInterval(mover);
 
-    }
-    if(counter >=3){
-        counter=-1
-    }
-    console.log(counter);
-changer();
+  counter--;
+  img[0].src = images[counter];
+  if (counter <= -1) {
+    counter = 2;
+  }
+  if (counter >= 3) {
+    counter = -1;
+  }
+
+  changer();
 }
